798db8070fcc4df035fdabc96b1ce56fad148def
michael <michael.kusdya@gmail.com>
2018-03-23 16:54:22 +0700

    first

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: 'The Book Thief'},
+        {title: 'Sapiens'},
+        {title: 'Meditations'},
+        {title: 'The Kite Runner'}
+    ]
+}
